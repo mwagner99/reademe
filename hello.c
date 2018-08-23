ce86c34ad825bc91d2cb68fc0d1c290579b2d5fd
mwagner99 <42562660+mwagner99@users.noreply.github.com>
2018-08-23 08:16:54 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Mark Wagner AJ Appleget
+ * Date: 2016/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mark Wagner Mechanical Engineering\n");
+
+  return 0;
+}
